6fa6f2e3d5262e94c46165768f096157ec71b002
larytet <larytet@yahoo.com>
2016-10-19 18:15:06 +0300

    BSOD dump header update for 64 bits:modules

--- parse_minidump.py
+++ parse_minidump.py
@@ -184,6 +184,28 @@ EXCEPTION_RECORD64_STRUCT = (
     DataField("__unusedAlignment", 4),
     DataField("ExceptionInformation", 8*EXCEPTION_MAXIMUM_PARAMETERS)
 );
+
+DUMP_0x2000_STRUCT = (
+    DataField("Uknwn", 4),  # 2000
+    DataField("Uknwn", 4),  
+    DataField("Uknwn", 8),
+    
+    DataField("Uknwn", 4), # 2010
+    DataField("Uknwn", 4),
+    DataField("Uknwn", 4),
+    DataField("Uknwn", 4),
+
+    DataField("Uknwn", 4), # 2020
+    DataField("Uknwn", 4),
+    DataField("Uknwn", 4),
+    DataField("Uknwn", 4),
+
+    DataField("Uknwn", 4), # 2030
+    DataField("Uknwn", 4),
+    DataField("Strings", 4),
+    DataField("Uknwn", 4),
+);
+
                                     
 HEADER64_STRUCT = (
     DataField("Signature", 4),
@@ -218,46 +240,52 @@ HEADER64_STRUCT = (
     DataField("Unused2", 2),
     DataField("Reserved", 0xfb4),
     # Offset  0x2000  
+    DataField("DUMP_0x2000_STRUCT", 4, DUMP_0x2000_STRUCT),
 );
 
 VS_FIXEDFILEINFO_STRUCT = (
-  DataField("dwSignature", 4),
-  DataField("dwStrucVersion", 4),
-  DataField("dwFileVersionMS", 4),
-  DataField("dwFileVersionLS", 4),
-  DataField("dwProductVersionMS", 4),
-  DataField("dwProductVersionLS", 4),
-  DataField("dwFileFlagsMask", 4),
-  DataField("dwFileFlags", 4),
-  DataField("dwFileOS", 4),
-  DataField("dwFileType", 4),
-  DataField("dwFileSubtype", 4),
-  DataField("dwFileDateMS", 4),
-  DataField("dwFileDateLS", 4)
+    DataField("dwSignature", 4),
+    DataField("dwStrucVersion", 4),
+    DataField("dwFileVersionMS", 4),
+    DataField("dwFileVersionLS", 4),
+    DataField("dwProductVersionMS", 4),
+    DataField("dwProductVersionLS", 4),
+    DataField("dwFileFlagsMask", 4),
+    DataField("dwFileFlags", 4),
+    DataField("dwFileOS", 4),
+    DataField("dwFileType", 4),
+    DataField("dwFileSubtype", 4),
+    DataField("dwFileDateMS", 4),
+    DataField("dwFileDateLS", 4)
 );
 
 MINIDUMP_LOCATION_DESCRIPTOR = (
-  DataField("DataSize", 4),
-  DataField("Rva", 4)
+    DataField("DataSize", 4),
+    DataField("Rva", 4)
 );
 
 MINIDUMP_MODULE64_STRUCT = (
-  DataField("BaseOfImage", 8),
-  DataField("SizeOfImage", 4),
-  DataField("CheckSum", 4),
-  DataField("TimeDateStamp", 4),
-  DataField("ModuleNameRva", 4),
-  DataField("VersionInfo", 4, VS_FIXEDFILEINFO_STRUCT),
-  DataField("CvRecord", 4, MINIDUMP_LOCATION_DESCRIPTOR),
-  DataField("MiscRecord", 4, MINIDUMP_LOCATION_DESCRIPTOR),
-  DataField("Reserved0", 8),
-  DataField("Reserved1", 8),
+    DataField("BaseOfImage", 8),
+    DataField("SizeOfImage", 4),
+    DataField("CheckSum", 4),
+    DataField("TimeDateStamp", 4),
+    DataField("ModuleNameRva", 4),
+    DataField("VersionInfo", 4, VS_FIXEDFILEINFO_STRUCT),
+    DataField("CvRecord", 4, MINIDUMP_LOCATION_DESCRIPTOR),
+    DataField("MiscRecord", 4, MINIDUMP_LOCATION_DESCRIPTOR),
+    DataField("Reserved0", 8),
+    DataField("Reserved1", 8),
 );
 
 MINIDUMP_MODULE_LIST_STRUCT = (
-  DataField("NumberOfModules", 4),
-  DataField("Modules", 4, MINIDUMP_MODULE64_STRUCT)
+    DataField("NumberOfModules", 4),
+    DataField("Modules", 4, MINIDUMP_MODULE64_STRUCT)
 );
+
+DUMP_0x2000_STRINGS_STRUCT = (
+    DataField("Length", 4),
+    DataField("String", 4),
+); 
 
 def read_field(file, size):
     data = file.read(size)
@@ -316,6 +344,37 @@ def parse_dump_header_physical_blocks_32(arguments, file_dump):
     number_of_runs = parse_field(file_dump, PHYSICAL_MEMORY_DESCRIPTOR32_STRUCT[0])
     number_of_pages = parse_field(file_dump, PHYSICAL_MEMORY_DESCRIPTOR32_STRUCT[1])
 
+def parse_dump_header_0x2000(arguments, file_dump):
+    strings_offset = None
+    for data_field in DUMP_0x2000_STRUCT:
+        (value, contains_ascii, value_ascii) = parse_field(file_dump, data_field)
+        if (data_field.name == "Strings"):
+            strings_offset = int(value, 0x16)
+            logger.debug("Loaded modules names at offset {0}".format(hex(strings_offset)))
+    return (strings_offset)
+
+def parse_strings(arguments, file_dump, strings_offset):
+    # End of the strings section is 16 bits zero
+    while (True):
+        file_offset = file_dump.tell()
+        data = read_field(file_dump, 4)
+        length_hex = data_to_hex(data)
+        length = int(length_hex, 16)
+        if (length == 0):
+            break
+        if (length > 128):
+            break
+        string = read_field(file_dump, length)
+        (contains_ascii, value_ascii) = data_to_ascii(data)
+        logger.debug("{0}: '{1}'".format(hex(file_offset, value_ascii)))
+        data = read_field(file_dump, 2) # I expect zero terminaiton here
+        zero_hex = data_to_hex(data)
+        zero = int(zero_hex, 16)  
+        if (zero != 0):
+            logger.debug("Missing zero termination - got {0} instead".format(hex(zero_hex)))
+            break
+        (contains_ascii, value_ascii) = data_to_ascii(data)
+        
 
 def parse_dump_header_64(arguments, file_dump):
     logger.info("64bits dump")
@@ -335,6 +394,9 @@ def parse_dump_header_64(arguments, file_dump):
             elif (data_field.name == "Exception"):
                 (exception_code, exception_flags, exception_address) = parse_dump_header_exception_64(arguments, file_dump)
                 logger.info("Exception: code={0}, address={1}, flags={2}".format(hex(exception_code), hex(exception_address), hex(exception_flags)))
+            elif (data_field.name == "DUMP_0x2000_STRUCT"):
+                strings_offset = parse_dump_header_0x2000(arguments, file_dump)
+                parse_strings(arguments, file_dump, strings_offset)
             else:
                 parse_dump_header_generic_struct(arguments, file_dump, data_field.data_struct)
     return physical_memory_presents;
