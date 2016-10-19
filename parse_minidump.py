9e394f8b6547d19be82be769d8c5f40a22e13636
larytet <larytet@yahoo.com>
2016-10-20 01:10:42 +0300

    BSOD dump header update for 64 bits: loaded modules

--- parse_minidump.py
+++ parse_minidump.py
@@ -5,6 +5,16 @@
 '''
 The script parses the Windows BSOD dumpfiles. It handles only files which start with 
 'PAGEDU64' or 'PAGEDUMP'
+Example of output
+INFO:parser:Parse file '../winvm/minidump.dmp'
+INFO:parser:64bits dump
+INFO:parser:Exception: code=0x80000003, address=0xfffff800026d4f00L, flags=0x1
+.......................
+INFO:parser:Stack: 0xfffff88001127c0bL, \.S.y.s.t.e.m.R.o.o.t.\.s.y.s.t.e.m.3.2.\.d.r.i.v.e.r.s.\.f.l.t.m.g.r...s.y.s.......
+.......................
+INFO:parser:Stack: 0xfffff800026d4f00L, \.S.y.s.t.e.m.R.o.o.t.\.s.y.s.t.e.m.3.2.\.n.t.o.s.k.r.n.l...e.x.e...
+.......................
+
 Usage:
     parse_minidump.py parse --filein=FILENAME 
 
